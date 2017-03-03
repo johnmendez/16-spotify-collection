395f1458b892802ac50f2e1889a8f11ad2ca9ab1
johnmendez <john.a.mendez2012@gmail.com>
2017-03-03 13:57:56 -0600

    Needs bug fixing for yarn start

---
+++ app/app.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <div class="app">
+    <div class="section panel">
+      <p class="panel-heading"></p>
+      <ul>
+      <song-item v-for="item in tracks" v-bind:track="item"></song-item>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import SongItem from './song-item.vue';
+export default {
+  components: { SongItem, },
+
+  created() {
+    fetch('https://api.spotify.com/v1/search?query=katy&type=track&offset=0&limit=20')
+  .then(r => r.json())
+  .then((data) => {
+    this.track = data;
+  });
+  },
+
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
---
+++ app/song-item.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div class="results">
+    <div class="track">
+      <div class="track__media">
+        <div class="track__frame"></div>
+        <div class="song-name">
+          <div class="track__artist"></div>
+          <div class="track__name"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['track'],
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
